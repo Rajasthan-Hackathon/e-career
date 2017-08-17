8ba9e31d28b120aced91e194b2f911ca38d52890
Suyog Gadhave <itssuyog96@gmail.com>
2017-08-17 19:23:54 +0530

    Registration Completed

--- src/app/register/user-register/user-register.component.ts
+++ src/app/register/user-register/user-register.component.ts
@@ -1,6 +1,13 @@
+import { Subject } from 'rxjs';
+import { Observable } from 'rxjs/Rx';
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
 import { AngularFireDatabase } from "angularfire2/database";
+import { AngularFireAuth } from "angularfire2/auth";
+import { Router } from "@angular/router";
+// import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/map';
+
 @Component({
   selector: 'app-user-register',
   templateUrl: './user-register.component.html',
@@ -14,17 +21,46 @@ export class UserRegisterComponent implements OnInit {
   private isHSC: boolean = false;
   private isG: boolean = false;
   private isPG: boolean = false;
-  constructor(private db: AngularFireDatabase) { }
+  private skills: any[];
+  private subject = new Subject<any>();
+  private requestAutocompleteItems: any;
+  constructor(private db: AngularFireDatabase, private af: AngularFireAuth, private router: Router) {
+    this.requestAutocompleteItems =
+      this.db.list('skills', {
+        query: {
+          orderByChild: 'name',
+          startAt: this.subject,
+          limitToFirst: 2
+        }
+      });
+
+    this.requestAutocompleteItems.subscribe(res => {
+      console.log(res);
+    })
+  }
+
 
   ngOnInit() {
 
   }
 
-  public requestAutocompleteItems = (text: string): Observable<any[]> => {
-    return this.db.list('/skills').map(x => {
-      return x;
+  public register(form) {
+    this.af.auth.createUserWithEmailAndPassword(form.email, form.pass).then(res => {
+      form.pass = null;
+      form.uid = res.uid;
+      this.db.list('User').push(form).then(x => {
+        this.router.navigate(['../login/user']);
+      })
     })
-  };
+  }
+
+  public onAdd(val) {
+    //this.skills.push(val);
+  }
+
+  public onRemove(val) {
+
+  }
 
   public edu_filter(val) {
     console.log(val);
@@ -67,4 +103,11 @@ export class UserRegisterComponent implements OnInit {
     }
   }
 
+  call(inp) {
+    console.log(inp);
+
+    this.subject.next(inp);
+  }
+
 }
+
