2670b656267691bb1bfd56bba7d237529ffd28fe
Suyog Gadhave <itssuyog96@gmail.com>
2017-08-17 22:28:59 +0530

    coy profile

--- src/app/dashboard/coy-profile/coy-profile.component.ts
+++ src/app/dashboard/coy-profile/coy-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from "angularfire2/database";
 
 @Component({
   selector: 'app-coy-profile',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoyProfileComponent implements OnInit {
 
-  constructor() { }
+  private coy = {};
+  constructor(private db: AngularFireDatabase) { }
 
   ngOnInit() {
+    this.db.list('Company', {
+      query: {
+        orderByChild: 'uid',
+        equalTo: "KT5ZwLyK8YZWwHrYQhvkmZtKSGN2"
+      }
+    }).subscribe(res => {
+      console.log(res);
+      this.coy = res[0];
+    })
   }
 
 }
